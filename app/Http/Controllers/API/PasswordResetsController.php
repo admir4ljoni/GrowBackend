5969efb8be828523b9dd92be10b7ceb55a82b5c1
Daniswara <akuncadangannn2020@gmail.com>
2024-11-10 19:58:25 +0700

    a

--- app/Http/Controllers/API/PasswordResetsController.php
+++ app/Http/Controllers/API/PasswordResetsController.php
@@ -48,7 +48,7 @@ class PasswordResetsController extends Controller
             'otp' => $otp,
             'is_verified' => false,
         ]);
-        
+
         Mail::to($request->email)->send(new OTPMail($otp));
 
         return response()->json(['message' => 'We have sent an OTP to your email.']);
@@ -82,7 +82,7 @@ class PasswordResetsController extends Controller
 
         return response()->json(['message' => 'Your password has been changed!']);
     }
-    
+
     public function verifyOTP(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -111,46 +111,45 @@ class PasswordResetsController extends Controller
 
         return response()->json(['message' => 'OTP verified successfully. You can now reset your password.']);
     }
-    
+
     public function resendOTP(Request $request)
     {
         $email = $request->email ?? session('otp_email');
-    
+
         // Periksa apakah email disediakan dari request atau session
         if (!$email) {
             return response()->json(['error' => 'No email found for OTP resend. Please provide an email or request OTP again.'], 400);
         }
-    
+
         // Cari user berdasarkan email
         $user = User::where('email', $email)->first();
-    
+
         if (!$user) {
             return response()->json(['error' => 'User not found.'], 404);
         }
-    
+
         // Jika user sudah terverifikasi, tetapi sedang dalam proses reset password, kirim OTP untuk reset password
         if ($user->is_verified && $request->type === 'reset-password') {
             $otp = rand(1000, 9999);
             $user->otp = $otp;
             $user->save();
-    
-            Mail::to($user->email)->send(new OTPMail($otp));
-            
+
+            Mail::to($user->email)->send(new OTPMail($otp, $user->name));
+
             return response()->json(['message' => 'OTP for password reset has been sent to your email.'], 200);
         }
-    
+
         // Jika user belum terverifikasi, kirim OTP untuk verifikasi akun
         if (!$user->is_verified) {
             $otp = rand(1000, 9999);
             $user->otp = $otp;
             $user->save();
-    
-            Mail::to($user->email)->send(new OTPMail($otp));
-            
+
+            Mail::to($user->email)->send(new OTPMail($otp, $user->name));
+
             return response()->json(['message' => 'OTP for account verification has been sent to your email.'], 200);
         }
-    
+
         return response()->json(['message' => 'No action needed. User is already verified and not in reset password process.'], 400);
     }
-    
 }
